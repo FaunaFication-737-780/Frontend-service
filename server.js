b431d7cd2d330f7b0f60e99cec600844a85b28a9
Bryce Wilkinson <bjw123@users.noreply.github.com>
2021-04-30 12:45:25 +0900

    db connection
    Template for db connection. Although we will try migrate this to a FaaS when we have a chance

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var cfenv = require("cfenv");
 var bodyParser = require('body-parser')
 const request = require('request');
-
+const mongoose = require("mongoose")
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,6 +15,15 @@ app.use(bodyParser.json())
 const routes = require("./routes")
 app.use(routes)
 
+mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+db.once('open', function() {
+    // we're connected!
+    console.log("database connected")
+
+});
 
 
 
