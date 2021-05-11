03361e734492c3c3a0dc8b41fba322a27d5c3a59
Ray Guo <ma2808203259@hotmail.com>
2021-05-11 15:19:10 +0800

    example livedata for mongodb, will updata to backend server repo later

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ app.use(bodyParser.json())
 const routes = require("./routes")
 app.use(routes)
 
-
-mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
+//mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 let db = mongoose.connection;
 
@@ -32,10 +33,6 @@ db.once('open', function() {
     console.log("database connected")
 
 });
-
-
-
-
 
 
 
