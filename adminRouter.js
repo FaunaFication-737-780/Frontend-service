c94851c52b2742b6827e9dfe633eef6c6303a191
Ray Guo <ma2808203259@hotmail.com>
2021-05-19 23:07:07 +0800

    Praff's work reformated

---
+++ adminRouter.js
@@ -0,0 +1,37 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
+const User = require('./models/MongooseSchema')
+const mongoose = require('mongoose')
+
+
+
+AdminBro.registerAdapter(AdminBroMongoose)
+
+
+const AdminBroOptions = {
+    //resources: [cat],
+    preventAssignment: true,
+    databases: [mongoose]
+  }
+
+const ADMIN = {
+    email: process.env.ADMIN_EMAIL|| "admin@example.com",
+    password: process.env.ADMIN_PASSWORD|| "123456"
+}
+
+const adminBro = new AdminBro(AdminBroOptions)
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+    cookieName : process.env.ADMIN_COOKIE_NAME || "admin",
+    cookiePassword: process.env.ADMIN_COOKIE_PASSWORD|| "12345678",
+    authenticate: async(email,password) =>{
+        if(email == ADMIN.email&&password == ADMIN.password){
+            return ADMIN
+        }
+        return null
+    }
+})
+
+
+module.exports = router
