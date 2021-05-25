5111522391e0d60705086c3dbbdc92443d688d38
Bryce Wilkinson <bjw123@users.noreply.github.com>
2021-05-25 13:13:13 +0900

    fix lealfet issues with cards and reinitizialization

--- views/js/species-cards.js
+++ views/js/species-cards.js
@@ -1,15 +1,88 @@
+//init sidenav bar that displays cards
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
 });
 
+
+//convert string to camel case used to convert SpeciesIntro name into referencable format in API
+function camelize(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+}
+
+//set up the map
+let map = new L.map('mapid').setView([-27.833, 133.583], 4);
+//gets geodata and displays on leaflet
+const getData = (speciesName) => {
+    var geoData;
+    $.get('/mapData', {
+        //only for showing an example
+        name: speciesName
+        //name: 'agileWallaby'
+    }, function (data) {
+        console.log(data);
+        geoData = data.data
+        //set up the map
+        var container = L.DomUtil.get('mapid');
+        if(container != null){
+            container._leaflet_id = null;
+        }
+
+        let map = new L.map('mapid').setView([-27.833, 133.583], 4);
+        // create boundary box
+        var boundaryBox = data.data.features[0].geometry.bbox
+        console.log('bbox is ' + boundaryBox);
+        var fixedBox = []
+
+
+        boundaryBox.forEach(element => {
+            console.log('box' + element);
+
+        });
+        fixedBox = [
+            [boundaryBox[1], boundaryBox[0]],
+            [boundaryBox[3], boundaryBox[2]]
+        ]
+        console.log('fixed box' + typeof fixedBox);
+
+
+
+        //create the map
+        L.tileLayer(
+            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1
+            }).addTo(map);
+        //add geojson file
+
+        L.geoJson(geoData).addTo(map)
+        //fit the boundary
+        map.fitBounds(fixedBox)
+    })
+
+
+}
+
+
 $(document).ready(() => {
 
     console.log('DOM is loaded')
-    document.addEventListener('DOMContentLoaded', function() {
+
+
+    /*document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.collapsible');
         var instances = M.Collapsible.init(elems, options);
     });
+
+     */
+
+    //init tabs
     $('.tabs').tabs();
 
 
@@ -57,6 +130,9 @@ $(document).ready(() => {
 
 
 
+
+
+
         //Sets cookie to onclick to pass to next page
         var element= document.getElementsByClassName('species-redirect');
         for(let i=0;i<element.length;i++){
@@ -67,13 +143,18 @@ $(document).ready(() => {
                 //console.log(document.cookie)
                 //console.log(myParent)
                 let species = myParent.firstElementChild.innerHTML
+                //document.getElementById('mapid').innerHTML = "";
 
+                getData(camelize(species))
 
                 json[0].forEach(element => {
                     if (species == element.name)
                     {
                         document.getElementById("species-name").innerText = element.name
                         document.getElementById("species-pic").src = element.image
+
+
+
                     }
                 })
 
