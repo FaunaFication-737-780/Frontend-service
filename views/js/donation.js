ed839cc6a33a6f7446c1c663a5b92977fd31856f
Ray Guo <ma2808203259@hotmail.com>
2021-05-26 17:21:40 +0800

    error message

--- views/js/donation.js
+++ views/js/donation.js
@@ -1,31 +1,37 @@
-const stripePayment = async (radios) => {
+const stripePayment = async () => {
     var priceID
     var payMode = 'payment'
+    var radios = document.getElementsByName('amount');
+    console.log(radios);
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-            
+
             priceID = radios[i].value
             // only one radio can be logically checked, don't check the rest
             break;
         }
     }
 
+
     if ($('#monthCheckbox').is(':checked') && priceID == 'price_1GtQGvECm5TjDc1IFaRIJpCx') {
         payMode = 'subscription'
     }
-   
+
     var stripe = Stripe('pk_test_853qy8se4d90x2LxszV5GAi700pL7qNzqY');
     stripe.redirectToCheckout({
-        lineItems: [{price: priceID, quantity: 1}],
-        mode: payMode,
-        // Do not rely on the redirect to the successUrl for fulfilling
-        // purchases, customers may not always reach the success_url after
-        // a successful payment.
-        // Instead use one of the strategies described in
-        // https://stripe.com/docs/payments/checkout/fulfillment
-        successUrl: 'https://localhost:3000/success.html',
-        cancelUrl: 'https://localhost:3000/canceled.html',
-    })
+            lineItems: [{
+                price: priceID,
+                quantity: 1
+            }],
+            mode: payMode,
+            // Do not rely on the redirect to the successUrl for fulfilling
+            // purchases, customers may not always reach the success_url after
+            // a successful payment.
+            // Instead use one of the strategies described in
+            // https://stripe.com/docs/payments/checkout/fulfillment
+            successUrl: 'https://localhost:3000/success.html',
+            cancelUrl: 'https://localhost:3000/canceled.html',
+        })
         .then(function (result) {
             if (result.error) {
                 // If `redirectToCheckout` fails due to a browser or network
@@ -36,13 +42,57 @@ const stripePayment = async (radios) => {
         });
 }
 
+const checkInput = () => {
+
+   
+
+    if ($('#first_name').val() == "" || $('#last_name').val() == "" || $('#email').val() == "") {
+
+        if ($('#first_name').val() == "") {
+            $("#first_name").removeClass("valid");
+            $("#first_name").addClass("invalid");
+            $("#firstNameLabel").css('color', 'red')
+        } else {
+            $("#first_name").removeClass("invalid");
+            $("#first_name").addClass("valid");
+            $("#firstNameLabel").css('color', '#00bfa5')
+        }
+
+        if ($('#last_name').val() == "") {
+            $("#last_name").removeClass("valid");
+            $("#last_name").addClass("invalid");
+            $("#lastNameLabel").css('color', 'red')
+        } else {
+            $("#last_name").removeClass("invalid");
+            $("#last_name").addClass("valid");
+            $("#lastNameLabel").css('color', '#00bfa5')
+        }
+
+        if ($('#email').val() == "") {
+            $("#email").removeClass("valid");
+            $("#email").addClass("invalid");
+            $("#emailLabel").css('color', 'red')
+        } else {
+            $("#email").removeClass("invalid");
+            $("#email").addClass("valid");
+            $("#emailLabel").css('color', '#00bfa5')
+        }
+    }else{
+        stripePayment()
+    }
+}
+
 
 $(document).ready(() => {
-    var radios = document.getElementsByName('amount');
-    console.log(radios);
+    $("#firstNameLabel").css('color', '#00bfa5')
+    $("#lastNameLabel").css('color', '#00bfa5')
+    $("#emailLabel").css('color', '#00bfa5')
+    
 
     $("#donationBTN").click(function () {
-        stripePayment(radios)
+        //stripePayment(radios)
+        checkInput()
+
     })
 
 })
