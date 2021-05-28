8c5376f51e4f8bc7a029e1274ea76ebccffcf56b
Bryce Wilkinson <bjw123@users.noreply.github.com>
2021-05-28 12:07:15 +0900

    hookup admin bro with data
    did with ruifeng. Strange error where is referring to a different collection. for now will change to this collection and if breaks will change back

--- models/MongooseSchema.js
+++ models/MongooseSchema.js
@@ -5,7 +5,7 @@ let SpeciesSchema = new Schema({
     name: String, population: Number, geoData: Array, about:String
 })
 
-const SpeciesModel = mongoose.model('species', SpeciesSchema);
+const SpeciesModel = mongoose.model('speciesInfo', SpeciesSchema);
 
 //generic error handler
 function handleError(err) {
