85b0b54af34bf2cdb83395d5967dface0dcbce70
Bryce Wilkinson <bjw123@users.noreply.github.com>
2021-05-30 11:31:36 +0900

    add dot.env for application security

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 const socket = require('./sockets')
 const adminRouter = require('./adminRouter')
+require('dotenv').config();
 
 socket.openSocket(io,app)
 
@@ -28,7 +29,9 @@ app.use(express.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(express.json())
-const uri = 'mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority'
+//const uri = 'mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority`
+
 //const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
 //mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -45,6 +48,7 @@ db.once('open', function() {
 
 
 // load local VCAP configuration  and service credentials
+/*
 var vcapLocal;
 try {
   vcapLocal = require('./vcap-local.json');
@@ -55,6 +59,7 @@ const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
+ */
 
 
 //serve static file (index.html, images, css)
